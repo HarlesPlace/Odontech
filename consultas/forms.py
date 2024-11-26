29b1e319283b602efa6a96f14d62a9d6b14dbfcc
Pietro Luigi Baldan Carbone <carbutops@gmail.com>
2024-11-25 23:27:06 -0300

    agendar consulta agora mostra os nomes e não como object(x)

--- consultas/forms.py
+++ consultas/forms.py
@@ -4,14 +4,34 @@ from pacientes.models import *
 from funcionarios.models import *
 from procedimentos.models import *
 
+from django import forms
+from .models import Cliente, Dentista, Consulta, Procedimento
+
+class NomeModelChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return obj.nome
+
 class ConsultaForm(forms.ModelForm):
     class Meta:
         model = Consulta
         fields = ['data', 'hora', 'status', 'paciente', 'dentista', 'procedimentos']
 
-    paciente = forms.ModelChoiceField(queryset=Cliente.objects.all(), required=True)
-    dentista = forms.ModelChoiceField(queryset=Dentista.objects.all(), required=True)
-    procedimentos = forms.ModelMultipleChoiceField(queryset=Procedimento.objects.all(), required=True, widget=forms.CheckboxSelectMultiple)
+    paciente = NomeModelChoiceField(
+        queryset=Cliente.objects.all(),
+        required=True,
+        label="Paciente"
+    )
+    dentista = NomeModelChoiceField(
+        queryset=Dentista.objects.all(),
+        required=True,
+        label="Dentista"
+    )
+    procedimentos = forms.ModelMultipleChoiceField(
+        queryset=Procedimento.objects.all(),
+        required=True,
+        widget=forms.CheckboxSelectMultiple,
+        label="Procedimentos"
+    )
 
-    data = forms.DateField(widget=forms.SelectDateWidget)
-    hora = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}))
+    data = forms.DateField(widget=forms.SelectDateWidget, label="Data")
+    hora = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}), label="Hora")
