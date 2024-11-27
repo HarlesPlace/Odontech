a997a2e3e62921c7e0e570c5905b85d1765ce649
Pietro Luigi Baldan Carbone <carbutops@gmail.com>
2024-11-27 13:04:39 -0300

    tentando montar lista de dentistas decobrir pq ele da como vazia

--- contas/urls.py
+++ contas/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path('', LoginView.as_view(), name='login'),
     path('profile/', views.user_profile, name='profile'),
+    path('dentistas/', views.dentistas, name= 'dentistas'),
 ]
