51d02b42e0fd492e3afca9ad5dad9d6f70b4587d
Nicolas Sobral Cruz <nicolasunif@gmail.com>
2024-11-28 10:58:43 -0300

    Mais uma alteração no build.sh pra conseguir o deploy no render

--- build.sh
+++ build.sh
@@ -16,5 +16,5 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists() or \
-    User.objects.create_superuser( os.environ["DJANGO_SUPERUSER_USERNAME"] , os.environ["DJANGO_SUPERUSER_FNAME"],os.environ["DJANGO_SUPERUSER_LNAME"], os.environ["DJANGO_SUPERUSER_EMAIL"], os.environ["DJANGO_SUPERUSER_PASSWORD"])
+    User.objects.create_superuser( os.environ["DJANGO_SUPERUSER_USERNAME"], os.environ["DJANGO_SUPERUSER_EMAIL"], os.environ["DJANGO_SUPERUSER_PASSWORD"])
 EOF
