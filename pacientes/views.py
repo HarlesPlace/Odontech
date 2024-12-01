b5ee8244d6c2e4d2be0184aa189818313a5a77be
Pietro Luigi Baldan Carbone <carbutops@gmail.com>
2024-12-01 12:18:11 -0300

    arrumado edit client para n crashar

--- pacientes/views.py
+++ pacientes/views.py
@@ -2,17 +2,20 @@ from .models import Cliente
 from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 from django.views import generic
 from .forms import ClienteForm
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 # Create your views here.
 
-class UpdateCliente(LoginRequiredMixin, PermissionRequiredMixin,generic.UpdateView):
+class UpdateCliente(LoginRequiredMixin, PermissionRequiredMixin, generic.UpdateView):
     permission_required = 'pacientes.change_cliente'
-    model= Cliente
-    template_name="pacientes/updatePaciente.html"
-    form_class=ClienteForm
+    model = Cliente
+    template_name = "pacientes/updatePaciente.html"
+    form_class = ClienteForm
+
     def get_success_url(self):
-        return reverse('pacientes/detailPaciente.html', args=[self.object.pk])
+        # Use o nome correto do URL com o namespace
+        return reverse_lazy('pacientes:detailCliente', args=[self.object.pk])
+
     
 class DetailCliente(LoginRequiredMixin, PermissionRequiredMixin,generic.DetailView):
     permission_required = 'pacientes.view_cliente'
